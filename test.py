4bbd160efbe318820965e5e6dece59d9828e0398
MrrBosss <abdumuratovabduvali06@gmali.com>
2024-05-15 11:52:06 +0500

    changes made

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 # test.pyooo
 import re
-email = input("Enter customer email: ")
-print(re.match(r"^\S+@\S+\.\S+$", email))
+# email = input("Enter customer email: ")
+# print(re.match(r"^\S+@\S+\.\S+$", email))
+phone = input("Enter customer email: ")
+pattern = re.match(r"^[+]{1}(?:[0-9\\-\\(\\)\\/\\.]\\s?){6,15}[0-9]{1}$",phone)
+print(pattern)
