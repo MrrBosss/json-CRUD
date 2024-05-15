5c59f58d3bfc3df3b0f7d2c1b7246203fd1ea00f
MrrBosss <abdumuratovabduvali07@gmail.com>
2024-05-15 09:22:48 +0000

    changes made

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import json
 import os
 from pywebio.input import *
 from pywebio.output import *
+
 import re
 rule = r"^[A-Z0-9\._+ '\"-]+\@[A-Z0-9]+\.[A-Z0-9]+"       
 
@@ -17,32 +18,47 @@ def load_customers(file_name):
 def save_customers(customers, file_name):
     with open(file_name, 'w') as file:
         json.dump(customers, file)
-    
 
+# check validation
+def check_phonenumber(num):
+    pattern = re.match(r"^[+]{1}(?:[0-9\\-\\(\\)\\/" \
+              "\\.]\\s?){6,15}[0-9]{1}$",num)
+    if pattern is None:
+        return ("Invalid!")
+    else:
+        put_text("Valid")
+
+# check validation of email  
+def check_email(email):
+    x = re.match(r"^\S+@\S+\.\S+$", email)
+    if x is None:
+        return "Invalid"
+    else:
+        put_text("Valid")
+        
+   
 
 # Add a new customer
 def add_customer(customers, file_name):
-    id = input("Enter customer id number: ")
-    name = input("Enter customer name: ")
-    email = input("Enter customer email: ")
-    while True:
-        x = re.match(r"^\S+@\S+\.\S+$", email)
-        if x is None:
-            email = input("Re-Enter customer email: ")
-            put_text("Invalid!")
-        else:
-            put_text("Valid!")
-            break
-    phone = input("Enter customer phone number: ")
-    while True:
-        pattern = re.match(r"^[+]{1}(?:[0-9\\-\\(\\)\\/" \
-              "\\.]\\s?){6,15}[0-9]{1}$",phone)
-        if pattern is None:
-            phone = input("Re-Enter customer phone number: ")
-            put_text("Invalid!")
-        else:
-            put_text("Valid")
-            break
+    name = input("Enter customer name: ",type=TEXT)
+    email = input("Enter customer email: ",type=TEXT, validate=check_email)
+    #     x = re.match(r"^\S+@\S+\.\S+$", email)
+    #     if x is None:
+    #         email = input("Re-Enter customer email: ")
+    #         put_text("Invalid!")
+    #     else:
+    #         put_text("Valid!")
+    #         break
+    phone = input("Enter customer phone number: ",type=NUMBER,validate=check_phonenumber)
+    # while True:
+    #     pattern = re.match(r"^[+]{1}(?:[0-9\\-\\(\\)\\/" \
+    #           "\\.]\\s?){6,15}[0-9]{1}$",phone)
+    #     if pattern is None:
+    #         phone = input("Re-Enter customer phone number: ")
+    #         put_text("Invalid!")
+    #     else:
+    #         put_text("Valid")
+    #         break
     address = input("Enter customer address: ")
     customer = {"name": name, "email": email, "phone": phone, "address": address}
     customers.append(customer)
@@ -54,7 +70,6 @@ def update_customer(customers, file_name):
     email = input("Enter customer email: ")
     for customer in customers:
         if customer["email"] == email:
-            customer["id"] = input("Enter new customer id number: ")
             customer["name"] = input("Enter new customer name: ")
             customer["phone"] = input("Enter new customer phone number: ")
             customer["address"] = input("Enter new customer address: ")
@@ -79,7 +94,7 @@ def search_customer(customers):
     email = input("Enter customer email: ")
     for customer in customers:
         if customer["email"] == email:
-            put_text(f"Customer found! Customer id {id(customer)}")
+            put_text(f"Customer found! Customer id {customer['id']}")
             put_text(f"Name: {customer['name']}")
             put_text(f"Email: {customer['email']}")
             put_text(f"Phone: {customer['phone']}")
@@ -92,7 +107,7 @@ def display_customers(customers):
     for customer in customers:
         put_text("_________Here's customer information!_________")
         put_text("Customers:")
-        put_text(f"Customer id number: {id(customer)}")
+        put_text(f"Customer id number: {customer['id']}")
         put_text(f"Name: {customer['name']}")
         put_text(f"Email: {customer['email']}")
         put_text(f"Phone: {customer['phone']}")
